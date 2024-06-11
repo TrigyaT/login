6a42cce1b9f5e0400a875cc43ce240d878b50510
TrigyaT <thapa.trigya@gmail.com>
2024-06-11 17:02:32 +0545

    Add Login component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Components/Login/login';
+function App() {
+  return (
+    //navbar
+    //carousel
+    //footer
+    
+    <div>
+      <Login /> 
+    </div>
+  );
+}
+
+export default App;
